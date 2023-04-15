6bdfea41898a499c505a698a0974e94a048a9b5a
JiaanL <53369926+JiaanL@users.noreply.github.com>
2023-04-15 11:37:19 +0800

    Create solution.sql

---
+++ Q3/solution.sql
@@ -0,0 +1,10 @@
+select
+  o.owner_id,
+  o.owner_name,
+  count(distinct category_id) as different_category_count
+from category_article_mapping c 
+  inner join article a on a.article_id = c.article_id
+  inner join owner o on o.owner_id = a.owner_id
+  inner join category cy on cy.category_id = c.category_id
+group by owner_id
+order by count(distinct category_id) desc
